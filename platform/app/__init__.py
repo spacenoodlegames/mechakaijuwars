d174beabe694582d87c4897a1819f35ccb23ae87
Thomas Amundsen <spacemonkeygamesdev@gmail.com>
2014-11-11 06:11:53 +0000

    added 'my_service' endpoint that can respond to cross-domain web requests with a JSON response. this is all I need to be able to start implementing the servers

--- platform/app/__init__.py
+++ platform/app/__init__.py
@@ -2,8 +2,11 @@
 # all the imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+     abort, render_template, flash, make_response, current_app, jsonify
 from contextlib import closing
+import json
+from datetime import timedelta
+from functools import update_wrapper
 
 # configuration
 DATABASE = '/home/ec2-user/mechakaijuwars/platform/app/tmp/flaskr.db'
@@ -35,6 +38,47 @@ def teardown_request(exception):
 	db = getattr(g, 'db', None)
 	if db is not None:
 		db.close()
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route('/')
 def show_entries():
@@ -72,4 +116,7 @@ def logout():
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
 
-
+@app.route('/my_service', methods=['GET'])
+@crossdomain(origin='*')
+def my_service():
+	return jsonify(foo='cross domain ftwsies')
