3801efaa4169c78f022f07703216c4c176be9d6e
Thomas Amundsen <spacemonkeygamesdev@gmail.com>
2014-11-18 23:31:24 -0800

    add echos

--- deploy/deploy-prod-platform.sh
+++ deploy/deploy-prod-platform.sh
@@ -1,6 +1,10 @@
+echo "starting deployment"
+
 cd /home/ec2-user/eb/mechakaijuwars
 git pull origin master
 cd /home/ec2-user/eb/mechakaijuwars/platform/app
 git add .
 git commit -m "automated deployment by elastic beanstalk"
 git aws.push
+
+echo "ending deployment"
