dbf5de48959c847654c176d0851367540fce9c14
Thomas Amundsen <spacemonkeygamesdev@gmail.com>
2014-11-18 23:23:42 -0800

    use path to eb directory in ec2-user home directory

---
+++ deploy/deploy-prod-platform.sh
@@ -0,0 +1,6 @@
+cd /home/ec2-user/eb/mechakaijuwars
+git pull origin master
+cd /home/ec2-user/eb/mechakaijuwars/platform/app
+git add .
+git commit -m "automated deployment by elastic beanstalk"
+git aws.push
