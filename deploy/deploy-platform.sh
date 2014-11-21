10fd9e839e9e2a0bc010454e68d66ca9e81a6820
Thomas Amundsen <spacemonkeygamesdev@gmail.com>
2014-11-21 00:34:26 -0800

    update to ubuntu

---
+++ deploy/deploy-platform.sh
@@ -0,0 +1,9 @@
+eb stop
+eb init -f
+cd "/var/lib/jenkins/jobs/MKW Deploy Prod Platform/workspace"
+git pull origin master
+cd "/var/lib/jenkins/jobs/MKW Deploy Prod Platform/workspace/platform/app"
+git add .
+git commit -m "automated deployment by elastic beanstalk"
+git aws.push
+eb start
